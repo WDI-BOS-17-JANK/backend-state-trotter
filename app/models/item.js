f30b542d3e1bcf47852fabb06b6742550cf5a7da
nriser <riserika@gmail.com>
2017-05-20 14:26:46 -0400

    Add model file for item resource
    Copy 'example' model and change the schema to support application's
    minimal viable product.

---
+++ app/models/item.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  // item id created automatically
+  title: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  // isPublic: {
+  //   type: String,
+  //   required: true,
+  // },
+  status: { // For complete/incomplete
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    // required: true,
+  },
+  due_date: {
+    type: Date,
+    // required: true,
+  },
+  // gps: {
+  //   type: _____,
+  //   required: true,
+  // },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      let userId = (options.user && options.user._id) || false;
+      ret.editable = userId && userId.equals(doc._owner);
+      return ret;
+    },
+  },
+}); // if there's a user with an ID, then use that information, make sure that userID equals to userID of whoever that owns it, and then return what is being created.
+
+// this function gets the length of the text that it's called on. E.g. item.title.length
+// itemSchema.virtual('length').get(function length() {
+//   return this.text.length;
+// });
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
